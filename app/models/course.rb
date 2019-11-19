3b0db88c0b231f63f742030fa9df51cea5e8266e
Stephanie Voigt <stephanie.a.voigt@gmail.com>
2019-11-19 12:36:00 +0100

    Added validations in Course Model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,8 @@
 class Course < ApplicationRecord
+  validates :title, presence: true
+  validates :decription, presence: true
+  validates :url, uniqueness: true
+  validates :provider, uniqueness: true
+  validates :category, uniqueness: true
+  validates :duration, uniqueness: true
 end
