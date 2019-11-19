a81a8c04d1380138a3db49dd46ed9e6e096d3879
Stephanie Voigt <stephanie.a.voigt@gmail.com>
2019-11-19 11:23:01 +0100

    Added MVC for Courses

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_18_200124) do
+ActiveRecord::Schema.define(version: 2019_11_19_094153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "courses", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "url"
+    t.string "provider"
+    t.json "category"
+    t.integer "duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string "slug", null: false
