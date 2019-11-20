cf0303e41959558084bc2da20ce9eb6d8e827ccb
Willem Booysen <whbooysen@gmail.com>
2019-11-20 14:01:46 +0100

    sortable.js added - needs more work

---
+++ app/javascript/packs/sortable.js
@@ -0,0 +1,24 @@
+import Sortable from 'sortablejs';
+
+
+// Wait until page is loaded before runnning code:
+document.addEventListener("DOMContentLoaded", function() {
+  // console.log('Your document is ready!');
+
+  const sortableList = document.querySelectorAll('.sortable-list'); // Select ALL lists on page
+
+  if (sortableList.length == 0) return; // Don't run the rest of this function if there are no lists.
+
+  sortableList.forEach((list) => {
+    const element = list;
+    const groupName = element.dataset.groupname;
+
+    Sortable.create(element, {
+        group: {
+            name: groupName,
+            pull: 'clone' // To clone: set pull to 'clone'
+        },
+        animation: 150
+    });
+  })
+});
