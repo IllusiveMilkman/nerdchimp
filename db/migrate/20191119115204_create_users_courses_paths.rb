48bc2a9dfd82a4f34f30ab6cb4a8ec2785a419e2
Willem Booysen <whbooysen@gmail.com>
2019-11-21 10:41:18 +0100

    changes in seed columns and migrations due to acts_as_list

--- db/migrate/20191119115204_create_users_courses_paths.rb
+++ db/migrate/20191119115204_create_users_courses_paths.rb
@@ -1,7 +1,6 @@
 class CreateUsersCoursesPaths < ActiveRecord::Migration[5.2]
   def change
     create_table :users_courses_paths do |t|
-      # t.string :course_position # removing due to acts_as_list gem
       t.references :user_course, foreign_key: true
       t.references :path, foreign_key: true
 
