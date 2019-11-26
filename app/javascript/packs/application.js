d3173ecf6a9dc2b2164feefc8827448b35247462
Moritz von Buchwaldt <moritzvonbuchwaldt@web.de>
2019-11-26 14:58:20 +0100

    i mastered the footer

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,8 +4,8 @@ import "packs/copyurl.js";
 import "packs/sortable.js";
 import "packs/sweetalert.js";
 import "packs/progress.js";
-import "packs/bubbles.js";
 import "packs/scraper.js";
+import "packs/bubbles.js";
 
 
 
