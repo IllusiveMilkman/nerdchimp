73b4947b34bb70b28070d3d9545905c231f246a7
Abdelrahman Elzohairy <a.zohairy@gmail.com>
2019-11-28 07:53:01 +0100

    Create path progress bar

--- app/javascript/packs/progress.js
+++ app/javascript/packs/progress.js
@@ -2,12 +2,13 @@ const courseBananas = document.querySelectorAll('#coursebananas')
 const picker = document.querySelectorAll('#slider');
 picker.forEach(i => {
   i.addEventListener('change', (event) => {
-    fetch(`/users/${event.target.dataset.user}/user_courses/${event.target.dataset.course}`, {
+    fetch(`/users/${event.target.dataset.user}/user_courses/${event.target.dataset.usercourse}`, {
       method: "PATCH",
       body: JSON.stringify({
         usercourse: {
           course_tracker: parseInt(event.target.value)
-        }
+        },
+        path_id: event.target.dataset.path
       }),
       headers: {
         'Content-Type': 'application/json',
@@ -19,6 +20,19 @@ picker.forEach(i => {
         .then(data => {
           // Progress Data
           console.log(data)
+          // Path Progress
+          var td = document.querySelector('#total_duration')
+          var tt = document.querySelector('#total_tracker')
+          var tp = document.querySelector('#total_progress')
+          var cp = document.querySelector('#circle_progress')
+
+          td.innerHTML = `${data.total_duration} Hours in total`
+          tt.innerHTML = `${parseInt(data.total_tracker)} Hours is completed`
+          tp.innerHTML = `${data.progress} %`
+          cp.setAttribute('data-percentage',`${data.progress}`)
+
+
+          //Course Progress
           const progress = document.querySelectorAll(`#progress-${data.id}`)
           progress.forEach(i => {
             i.innerHTML = `<strong>${parseInt(data.course.duration - data.course_tracker)} hour(s) left</strong>`
--- app/models/users_courses_path.rb
+++ app/models/users_courses_path.rb
@@ -1,4 +1,5 @@
 class UsersCoursesPath < ApplicationRecord
   belongs_to :user_course
   belongs_to :path
+
 end
